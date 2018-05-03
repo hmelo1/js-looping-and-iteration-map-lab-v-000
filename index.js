3505725b926b57315209feb7247e5dd0e5ec76fa
Eddie Melo <heriberto@melo.nyc>
2018-05-03 21:33:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ function lowerCaseDrivers(drivers){
 }
 
 function nameToAttributes(names){
-  let new_names = names.map(function (name, index){
-    return name + name[index+1]
-  })
-  return new_names
+
 }
