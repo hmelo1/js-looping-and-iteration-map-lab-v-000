f525fb30a4c122feff0478cba27038b7682b98a2
Eddie Melo <heriberto@melo.nyc>
2018-05-03 21:29:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function lowerCaseDrivers(drivers){
 
 function nameToAttributes(names){
   let new_names = names.map(function (name, index){
-
+    return name + nake[index+1]
   })
   return new_names
 }
