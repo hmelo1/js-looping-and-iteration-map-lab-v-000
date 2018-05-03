f7ed238e1a216b73933e54cc60f4098854422aa8
Eddie Melo <heriberto@melo.nyc>
2018-05-03 21:40:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function lowerCaseDrivers(drivers){
 }
 
 function nameToAttributes(names){
-
+  return names.map(function (string){
+    
+  })
 }
 
 const newEngineers = map(oldAccounts, function (account) {
